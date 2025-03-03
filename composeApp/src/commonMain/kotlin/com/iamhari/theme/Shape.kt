1a24ca27bf542d816c2529a2409369267c7a2fbd
hariprasad <hariprasadragupathy@gmail.com>
2025-03-03 12:14:51 +0530

    commented material3

--- composeApp/src/commonMain/kotlin/com/iamhari/theme/Shape.kt
+++ composeApp/src/commonMain/kotlin/com/iamhari/theme/Shape.kt
@@ -1,7 +1,7 @@
 package com.iamhari.theme
 
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Shapes
+//import androidx.compose.material3.Shapes
 import androidx.compose.ui.unit.dp
 
 object AppShapes {
